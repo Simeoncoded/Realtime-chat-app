9c3956b8222b15ddabcafb47972c1e29857324c3
Simeoncoded <simeonolawore07@gmail.com>
2023-06-07 23:31:31 -1100

    first commit

---
+++ public/src/utils/APIRoutes.js
@@ -0,0 +1,8 @@
+import SetAvatar from "../pages/SetAvatar";
+
+  const host  = "http://localhost:5000";
+  export const registerRoute = `${host}/api/auth/register`
+  export const loginRoute = `${host}/api/auth/login`
+  export const setAvatarRoute = `${host}/api/auth/setAvatar`
+  export const allUsersRoute = `${host}/api/auth/allusers`
+  
